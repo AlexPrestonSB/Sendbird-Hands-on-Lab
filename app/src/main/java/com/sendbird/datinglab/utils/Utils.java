13f0f57b437b0261454a8fd63d51bec649d15f9c
Alex Preston <alex.preston@AlexP-F0117.local>
2020-09-08 13:51:57 -0700

    More cleaning up. Removed unneccessary files from existing project. Changed some color schemes.

--- app/src/main/java/com/sendbird/datinglab/utils/Utils.java
+++ app/src/main/java/com/sendbird/datinglab/utils/Utils.java
@@ -25,42 +25,6 @@ public class Utils {
 
     private static final String TAG = "Utils";
 
-    public static List<Profile> loadProfiles(Context context){
-        try{
-            GsonBuilder builder = new GsonBuilder();
-            Gson gson = builder.create();
-            JSONArray array = new JSONArray(loadJSONFromAsset(context, "profiles.json"));
-            List<Profile> profileList = new ArrayList<>();
-            for(int i=0;i<array.length();i++){
-                Profile profile = gson.fromJson(array.getString(i), Profile.class);
-                profileList.add(profile);
-            }
-            return profileList;
-        }catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private static String loadJSONFromAsset(Context context, String jsonFileName) {
-        String json;
-        InputStream is;
-        try {
-            AssetManager manager = context.getAssets();
-            Log.d(TAG,"path "+jsonFileName);
-            is = manager.open(jsonFileName);
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
-    }
-
     public static Point getDisplaySize(WindowManager windowManager){
         try {
             if(Build.VERSION.SDK_INT > 16) {
